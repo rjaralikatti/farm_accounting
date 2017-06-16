842338c6973007efc0303793125f03d88b463e7d
rjaralikatti <Raju.Aralikatti@cerner.com>
2017-06-16 06:05:52 +0530

    Home page improvement

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -5,4 +5,8 @@ class Farm < ApplicationRecord
   has_many :transactions
   attr_accessor :email
   
+  def sum_transaction(trans_type)
+    transactions.where(trans_type: trans_type).sum(:amount)
+  end
+  
 end
